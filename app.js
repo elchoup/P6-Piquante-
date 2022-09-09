d7aafe00d058210d4f2648f49b4efdff65ea7c02
Matthieu Hertaut <matthieu.hertaut@gmail.com>
2022-09-09 16:15:17 +0200

    Correction images

--- app.js
+++ app.js
@@ -4,16 +4,26 @@ const path = require('path');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
 const app = express();
 require('dotenv').config();
 
-const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce');
+
+//Authorisation pour faire des requètes provenant d'une autre origine.
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
+    next();
+  });
+
 
 
 const apiLimiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limite chaque requète d'Ip à un total de 100
+	max: 150, // Limite chaque requète d'Ip à un total de 100
 	standardHeaders: true, // Retourne les infos rateLimite dans l'header 'rateLimit'
 	legacyHeaders: false, // Désactive le `X-RateLimit-*` headers
 })
@@ -25,20 +35,14 @@ mongoose.connect(process.env.MONGO_URI,
     .catch(() => console.log('Connection à MongoDB échouée !'));
 
 app.use(express.json());
-
-//Authorisation pour faire des requètes provenant d'une autre origine.
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
-
 app.use(morgan('combined'));
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(helmet());
 app.use('/api', apiLimiter);
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
-app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app;
